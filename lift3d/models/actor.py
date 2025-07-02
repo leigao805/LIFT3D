7c9a116b7e5562c53797f3d85267aa572b65b2ee
Layne G. <37572543+leigao805@users.noreply.github.com>
2025-07-02 23:15:42 +0800

    修复actor.py
    恢复被删的PointCloudGuidedBatchNormMLP

--- lift3d/models/actor.py
+++ lift3d/models/actor.py
@@ -128,6 +128,36 @@ class VisionGuidedBatchNormMLP(Actor):
         actions = self.policy_head(emb)
         return actions
 
+class PointCloudGuidedBatchNormMLP(Actor):
+
+    def __init__(
+        self,
+        point_cloud_encoder: nn.Module,
+        robot_state_dim: int,
+        action_dim: int,
+        policy_hidden_dims: List[int],
+        nonlinearity: str,
+        dropout_rate: float,
+    ):
+        super(PointCloudGuidedBatchNormMLP, self).__init__()
+        self.point_cloud_encoder = point_cloud_encoder
+        self.policy_head = BatchNormMLP(
+            input_dim=point_cloud_encoder.feature_dim + robot_state_dim,
+            hidden_dims=policy_hidden_dims,
+            output_dim=action_dim,
+            nonlinearity=nonlinearity,
+            dropout_rate=dropout_rate,
+        )
+        for param in list(self.policy_head.parameters())[-2:]:
+            param.data = 1e-2 * param.data
+
+    def forward(self, images, point_clouds, robot_states, texts):
+        # * Notice: normalize the input point cloud
+        point_clouds = PointCloud.normalize(point_clouds)
+        point_cloud_emb = self.point_cloud_encoder(point_clouds)
+        emb = torch.cat([point_cloud_emb, robot_states], dim=1)
+        actions = self.policy_head(emb)
+        return actions
 
 class TokenVoxelGraspActor(Actor):
     def __init__(
